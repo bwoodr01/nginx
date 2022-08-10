82e2727a0593b1b841d885ee40595b020fe50375
Ben Woodrum <woodrum.ben@gmail.com>
2022-08-09 23:28:12 -0400

    Enhancement 2 v2

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -1,4 +1,4 @@
 provider "kubernetes" {
-  config_context_cluster = "arn:aws:eks:us-east-1:836566418525:cluster/turo"
+  config_context_cluster = "arn:aws:eks:us-east-1:663118211814:cluster/tf-eks-test"
   config_path            = "~/.kube/config"
 }
